1da5f4d631f08e51ce14a78592e8e48f5ab70a15
Illya Komar <illya-komar@ukr.net>
2023-07-07 21:55:19 +0300

    feat(active): init

--- src/app/active/page.tsx
+++ src/app/active/page.tsx
@@ -1,3 +1,19 @@
 export default function Active() {
-  return <h1>Hello, Active page!</h1>;
+  return (
+    <div className="active-page">
+      <div className="course-page__name-container">
+        <span>Мої курси</span>
+      </div>
+      <div className="course-page__button-container">
+        <Button
+          type="primary"
+          shape="round"
+          icon={<PlusCircleOutlined className="icon" />}
+          className="course-page__button-connect"
+        >
+          Додати курс
+        </Button>
+      </div>
+    </div>
+  );
 }
