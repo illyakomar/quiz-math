1c88dab75af3ecc3e0ef19f045067f717d144a9e
Illya Komar <illya-komar@ukr.net>
2023-09-15 14:48:06 +0300

    fix: fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,32 @@
-import { getServerSession } from 'next-auth';
+import { Nunito } from "next/font/google";
+import { getServerSession } from "next-auth";
 
-import AuthProvider from '@/components/AuthProvider/AuthProvider';
-import { authOptions } from './api/auth/[...nextauth]/route';
+import AuthProvider from "@/components/AuthProvider/AuthProvider";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-import 'react-responsive-modal/styles.css';
+import "react-responsive-modal/styles.css";
 
-import './../styles/main.scss';
+import "./../styles/main.scss";
 
 export const metadata = {
-  title: 'Quiz math',
-  description: 'With love',
+  title: "Quiz math",
+  description: "With love",
 };
 
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
+const nunito = Nunito({
+  subsets: ["cyrillic", "latin"],
+  display: "swap",
+});
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const session = await getServerSession(authOptions);
 
   return (
-    <html lang='uk-UA'>
+    <html lang="uk-UA" className={nunito.className}>
       <body>
         <AuthProvider session={session}>
           <main>{children}</main>
