f720179353ed48cc93d23d6bf04580c5f501b1c8
Denis Tatsii <den.tacij@gmail.com>
2023-12-17 16:44:05 +0200

    feat(uiLoading): add loading ui to main pages with dual spinner

---
+++ src/app/(main)/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingDualSpinner from '@/components/loading/DualSpinner';
+
+export default function MainLoading() {
+  return (
+    <div className='loading'>
+      <LoadingDualSpinner />
+    </div>
+  );
+}
